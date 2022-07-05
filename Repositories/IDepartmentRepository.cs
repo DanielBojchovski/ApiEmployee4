ae43078ddc01e9e5227a4b1e9a9f3fe6e79f15c0
Daniel Bojchovski <106997266+DanielBojchovski@users.noreply.github.com>
2022-07-06 00:24:23 +0200

    Create IDepartmentRepository.cs

---
+++ Repositories/IDepartmentRepository.cs
@@ -0,0 +1,10 @@
+using ApiEmployee4.Models;
+
+namespace ApiEmployee4.Repositories
+{
+    public interface IDepartmentRepository
+    {
+        Task<IEnumerable<Department>> GetDepartments();
+        Task<Department> GetDepartment(int departmentId);
+    }
+}
