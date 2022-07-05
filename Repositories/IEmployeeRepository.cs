cbbde7f54eb539baa69ba0736f368c2cb96c8753
Daniel Bojchovski <106997266+DanielBojchovski@users.noreply.github.com>
2022-07-06 00:24:00 +0200

    Create IEmployeeRepository.cs

---
+++ Repositories/IEmployeeRepository.cs
@@ -0,0 +1,20 @@
+using ApiEmployee4.Models;
+
+namespace ApiEmployee4.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> SearchByName(string name);
+        Task<IEnumerable<Employee>> SearchBySkill(string skill);
+        Task<IEnumerable<Employee>> Get();
+        Task<Employee> Get(int employeeId);
+        Task<Employee> GetEmployeeByEmail(string email);
+        Task<Employee> Create(Employee employee);
+        Task<Employee> Update(Employee employee);
+        Task Delete(int employeeId);
+        decimal GetAverageSalary();
+        decimal GetTotalSumOfSalaries();
+        Task<IEnumerable<Employee>> GetSalariesBiggerThen(decimal salary);
+        Task<IEnumerable<Employee>> GetSalariesLessThen(decimal salary);
+    }
+}
